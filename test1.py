600512eee99a06d01ba8ceda928b8783805d18ba
wenke <417496897@qq.com>
2025-05-31 02:01:15 +0800

    commit

--- test1.py
+++ test1.py
@@ -1,4 +1,4 @@
 import numpy, skimage, PIL
-print(numpy.__version__)
-print(skimage.__version__)
-print(PIL.__version__)
+print(numpy.__version__)    # 2.0.1
+print(skimage.__version__)  # 0.25.2
+print(PIL.__version__)  # 11.2.1
---
+++ test2.py
@@ -0,0 +1,29 @@
+import numpy as np
+from PIL import Image, ImageFont, ImageDraw
+from PIL.ImageChops import add, subtract, multiply, difference, screen
+import PIL.ImageStat as stat
+from skimage.io import imread, imsave, imshow, show, imread_collection, imshow_collection
+from skimage import color, exposure, img_as_float, data
+from skimage.transform import SimilarityTransform, warp, swirl
+from skimage.util import invert, random_noise, montage
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+from scipy.ndimage import affine_transform, zoom
+# from scipy import misc
+import cv2
+import requests
+from io import BytesIO
+
+# 读取网络图像
+url = "https://www.manongbook.com/d/file/python/e17c6518b44b6591781903c81d6590f14.jpg"
+response = requests.get(url)
+img_pil = Image.open(BytesIO(response.content))
+
+print(img_pil.width, img_pil.height, img_pil.mode, img_pil.format, type(img_pil))
+# 453 340 RGB PNG <class 'PIL.PngImagePlugin.PngImageFile'>
+
+#从PIL Image转为numpy
+img_np = np.array(img_pil)
+plt.imshow(img_np) # 将图片放入图片显示窗口里
+plt.axis("off") # 关闭显示图片的坐标轴
+plt.show() # 弹出显示窗口
