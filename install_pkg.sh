f7c472adbb3ff84da3ad416a6bd5af3f8536e9db
wenke <417496897@qq.com>
2025-05-31 01:36:16 +0800

    commit

---
+++ install_pkg.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+# 安装所需的所有包
+pip install numpy
+pip install scipy
+pip install scikit-image
+pip install scikit-learn   
+pip install pillow   
+pip install SimpleITK   
+pip install opencv-python   
+pip install matplotlib
+echo "DONE."
---
+++ test1.py
@@ -0,0 +1,4 @@
+import numpy, skimage, PIL
+print(numpy.__version__)
+print(skimage.__version__)
+print(PIL.__version__)
