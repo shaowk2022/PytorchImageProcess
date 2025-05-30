56ff6f093bb82b66c3e60d5b45434fbacc992aad
wenke <417496897@qq.com>
2025-05-31 02:11:31 +0800

    commit

--- test2.py
+++ test2.py
@@ -1,16 +1,6 @@
 import numpy as np
-from PIL import Image, ImageFont, ImageDraw
-from PIL.ImageChops import add, subtract, multiply, difference, screen
-import PIL.ImageStat as stat
-from skimage.io import imread, imsave, imshow, show, imread_collection, imshow_collection
-from skimage import color, exposure, img_as_float, data
-from skimage.transform import SimilarityTransform, warp, swirl
-from skimage.util import invert, random_noise, montage
-import matplotlib.image as mpimg
+from PIL import Image
 import matplotlib.pyplot as plt
-from scipy.ndimage import affine_transform, zoom
-# from scipy import misc
-import cv2
 import requests
 from io import BytesIO
 
